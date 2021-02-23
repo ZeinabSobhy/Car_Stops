8368254d016904275075f77bfdaf68c7cd2373c8
Mohamed Gamal <Mohamed.Gamal@arabiansystems.com>
2021-02-23 23:40:13 +0200

    final rs

--- src/Components/Video/index.js
+++ src/Components/Video/index.js
@@ -3,6 +3,7 @@ import './video.scss';
 import Back from  '../../assets/backgrounds/red-luxury-sedan-road.jpg';
 import EA from '../../assets/Emirates Auction - We Can Reach You Anywhere.mp4';
 import VideoPlayer from 'react-simple-video-player';
+import {FormattedMessage} from "react-intl";
 
 const Video =  props =>   {
 return(
@@ -12,6 +13,12 @@ return(
                        width={560}
                        height={330}/>
 
+                       <div className="video_txt">
+                           <h3> <FormattedMessage id="startToday.videoSection.first" /></h3>
+                           <h4><FormattedMessage id="startToday.videoSection.second" /></h4>
+                           <h5><FormattedMessage id="startToday.videoSection.third" /></h5>
+                           <h6><FormattedMessage id="startToday.videoSection.fourth" /></h6>
+                       </div>
     </div>
 )
 }
