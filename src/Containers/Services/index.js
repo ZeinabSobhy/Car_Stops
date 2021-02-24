a544aa10aa3251d25ed20956e550a6c38e5ad0ad
Mohamed Gamal <Mohamed.Gamal@arabiansystems.com>
2021-02-25 00:57:51 +0200

    final

--- src/Containers/Services/index.js
+++ src/Containers/Services/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './services.scss';
-
+import {FormattedMessage} from "react-intl";
 
 
 const Services = props =>  {
     return (
         <section className="services">
             <div className="services_container">
-
+                <div className="services_container_title">
+                    <h3> <FormattedMessage id="services.headline" /></h3>
+                    <p> <FormattedMessage id="services.subtitle" /></p>
+                </div>
             </div>
 
         </section>
