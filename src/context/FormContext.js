f92aad72a7c7092f8b6590be7bc2dc4d41c57287
Mohamed Gamal <Mohamed.Gamal@arabiansystems.com>
2021-02-25 16:35:52 +0200

    for

--- src/context/FormContext.js
+++ src/context/FormContext.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const FormContext = React.createContext([{}, () => {}]);
 
 const FormProvider = ({ children }) => {
-    const [sentRequest, setSentRequest] = useState(false);
+    const [sentRequest, setSentRequest] = useState(null);
 
     return (
         <FormContext.Provider value={[sentRequest, setSentRequest]}>{children}</FormContext.Provider>
